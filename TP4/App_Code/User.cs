5eaed6885ec1defed79e97d90103b49f0b6c5e8e
Charlesfire <charlesfire2@hotmail.com>
2015-05-14 13:41:07 -0400

    Amélioration de la master page

---
+++ TP4/App_Code/User.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Description résumée de User
+/// </summary>
+public class User
+{
+	public string Name
+	{
+		get;
+		set;
+	}
+
+	public string Password
+	{
+		get;
+		set;
+	}
+
+	private int postCount = 0;
+	public int PostCount
+	{
+		get
+		{
+			return postCount;
+		}
+
+		set
+		{
+			if (value < 0)
+				throw new ArgumentException("Post count can't be lesser than 0.");
+			postCount = value;
+		}
+	}
+
+	public bool IsAdmin
+	{
+		get;
+		set;
+	}
+
+	public string Email
+	{
+		get;
+		set;
+	}
+
+	public string Avatar
+	{
+		get;
+		set;
+	}
+
+	public string Adresse
+	{
+		get;
+		set;
+	}
+
+	public User()
+	{
+		
+	}
+}
--- TP4/MasterPage.master.cs
+++ TP4/MasterPage.master.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,6 +10,68 @@ public partial class MasterPage : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+      lblErreurConnexion.Text = "";
+    }
 
+    protected void Page_PreRender()
+    {
+      if (Session["user"] != null)
+      {
+        pnlConnected.Visible = true;
+        pnlConnexion.Visible = false;
+      }
+      else
+      {
+        pnlConnected.Visible = false;
+        pnlConnexion.Visible = true;
+      }
+    }
+
+    protected void btnSeConnecter_Click(object sender, EventArgs e)
+    {
+      OleDbConnection myConnection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + Server.MapPath(@"Forum DB/Forum.accdb"));
+      OleDbCommand myAccessCommand = new OleDbCommand("SELECT [Password], IsBanned, PostCount, IsAdmin, Email, Avatar, Adresse FROM Users WHERE Username = '" + txtbPseudo.Text + "';", myConnection);
+
+      try
+      {
+        myConnection.Open();
+        OleDbDataReader myReader = myAccessCommand.ExecuteReader();
+        if (myReader.Read() && (string)myReader["Password"] == txtbPassword.Text)
+        {
+          if ((bool)myReader["IsBanned"])
+          {
+            lblErreurConnexion.Text = "Vous avez été banni.";
+          }
+          else
+          {
+            User user = new User();
+            user.Name = txtbPseudo.Text;
+            user.Password = txtbPassword.Text;
+            user.PostCount = int.Parse(myReader["PostCount"].ToString());
+            user.IsAdmin = (bool)myReader["IsAdmin"];
+            user.Email = (string)myReader["Email"];
+            user.Avatar = (string)myReader["Avatar"];
+            user.Adresse = (string)myReader["Adresse"];
+            Session["user"] = user;
+          }
+        }
+        else
+        {
+          lblErreurConnexion.Text = "La combinaison pseudo/mot de passe est invalide.";
+        }
+      }
+      catch (Exception ex)
+      {
+        lblErreurConnexion.Text = "Erreur: " + ex.Message;
+      }
+      finally
+      {
+        myConnection.Close();
+      }
+    }
+
+    protected void btnDeconnexion_Click(object sender, EventArgs e)
+    {
+      Session["user"] = null;
     }
 }
