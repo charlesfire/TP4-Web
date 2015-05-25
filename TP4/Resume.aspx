f1e41b3613f6b1b7e8bd2ba46226b3c51bb32c11
Charlesfire <charlesfire2@hotmail.com>
2015-05-24 22:35:13 -0400

    ajout page résumé

---
+++ TP4/Resume.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/MasterPage.master" CodeFile="Resume.aspx.cs" Inherits="Resume" %>
+
+<asp:Content ContentPlaceHolderId="Wrapper" runat="server">
+  <div id = "main">
+	
+		<asp:Label ID="lblResume" runat="server" CssClass="subTitles" Text="Résumé"></asp:Label>
+		<br />
+		Nous avons reçu,
+	
+		<asp:Label ID="lblPrenom" runat="server"></asp:Label>
+		&nbsp;, votre demande de réservation en date du
+		<asp:Label ID="lblDate" runat="server"></asp:Label>
+		pour les matchs suivants :<br />
+		<br />
+		<asp:Table ID="tbInscriptions" runat="server" BorderColor="Gray" BorderStyle="Solid" BorderWidth="1px" GridLines="Both" HorizontalAlign="Center">
+			<asp:TableRow runat="server" BackColor="Gray" Font-Bold="True" ForeColor="White">
+				<asp:TableCell runat="server">Événement</asp:TableCell>
+				<asp:TableCell runat="server">Jeu</asp:TableCell>
+				<asp:TableCell runat="server">Plancher</asp:TableCell>
+				<asp:TableCell runat="server">Date</asp:TableCell>
+				<asp:TableCell runat="server">Heure</asp:TableCell>
+			</asp:TableRow>
+		</asp:Table>
+	
+		<br />
+		<br />
+		<asp:Label ID="lblNote" runat="server" ForeColor="Red" Text="Note : Veuillez imprimer et conserver ce document. Il est la preuve que vous vous êtes bien inscrit aux match ci-dessus."></asp:Label>
+		<br />
+		<br />
+		<asp:Button ID="btnRetour0" runat="server" OnClick="btnRetour_Click" Text="Retour à la page des évènements" CssClass="button" PostBackUrl="~/PageÉvènement.aspx" />
+	
+		<asp:Button ID="btnRetour" runat="server" OnClick="btnRetour_Click" Text="Retour à l'acceuil" CssClass="button" />
+	
+	</div>
+</asp:Content>
---
+++ TP4/Resume.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.UI.WebControls;
+
+public partial class Resume : System.Web.UI.Page
+{
+  /// <summary>
+  /// Événement produit lors du chargement de la page
+  /// </summary>
+  /// <param name="sender"></param>
+  /// <param name="e"></param>
+  protected void Page_Load(object sender, EventArgs e)
+  {
+    User user = (User)Session["user"];
+    if (user == null)
+    {
+      Response.Redirect("tp4.aspx");
+    }
+
+    // On affiche le nom/prénom/numéro de participant du membre ayant souscrit à des matchs
+    lblPrenom.Text = user.Name;
+
+    // On affiche la date et l'heure actuelle
+    lblDate.Text = DateTime.Now.ToLongDateString() + " à " + DateTime.Now.ToShortTimeString();
+
+    // Si les incriptions existent...
+    if (user.inscriptions != null)
+    {
+      // on parcours les user.inscriptions
+      foreach (Inscription inscription in user.inscriptions)
+      {
+        // On ajoute une ligne au tableau
+        TableRow nouvelleLigne = new TableRow();
+
+        // On ajoute la cellule de l'évènement
+        TableCell nouvelleCellule = new TableCell();
+
+        // On ajoute la cellule du jeu
+        nouvelleCellule = new TableCell();
+        nouvelleCellule.Text = inscription.GetJeu();
+        nouvelleLigne.Cells.Add(nouvelleCellule);
+
+        // On ajoute la cellule du plancher
+        nouvelleCellule = new TableCell();
+        nouvelleCellule.Text = "#" + inscription.GetPlancher();
+        nouvelleLigne.Cells.Add(nouvelleCellule);
+
+        // On ajoute la cellule de l'heure
+        nouvelleCellule = new TableCell();
+        nouvelleCellule.Text = inscription.GetHeure().ToShortTimeString();
+        nouvelleLigne.Cells.Add(nouvelleCellule);
+
+        // On ajoute au tableau la nouvelle ligne
+        tbInscriptions.Rows.Add(nouvelleLigne);
+      }
+    }
+  }
+
+  /// <summary>
+  /// Événement produit lors d'un clique sur le boutton "Retour"
+  /// </summary>
+  /// <param name="sender"></param>
+  /// <param name="e"></param>
+  protected void btnRetour_Click(object sender, EventArgs e)
+  {
+    // On retourne à la page principale
+    Response.Redirect("tp4.aspx");
+  }
+}
